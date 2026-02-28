9e9e6c186736bcd45ad93a97f15c80849537b35f
pdsong <songpeidong@gmail.com>
2026-02-28 15:24:09 +0800

    Update scraper to run on fixed schedule (9, 12, 18, 21 BJT) without scraping on boot

--- lib/insight/scraper/worker.ex
+++ lib/insight/scraper/worker.ex
@@ -10,8 +10,8 @@ defmodule Insight.Scraper.Worker do
   alias Insight.News
   alias Insight.Scraper.HN
 
-  # 每小时执行一次 (毫秒)
-  @interval 60 * 60 * 1000
+  # 北京时间 9:00, 12:00, 18:00, 21:00 对应 UTC 的 1, 4, 10, 13
+  @target_utc_hours [1, 4, 10, 13]
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -19,29 +19,52 @@ defmodule Insight.Scraper.Worker do
 
   @impl true
   def init(_opts) do
-    Logger.info("HackerNews 爬虫调度器启动...")
-    # 启动时延迟 5 秒执行一次，后续每小时执行
-    Process.send_after(self(), :crawl, 5_000)
+    Logger.info("HackerNews 定时爬虫调度器启动...")
+    schedule_next_run()
     {:ok, %{}}
   end
 
   @impl true
   def handle_info(:crawl, state) do
-    Logger.info("开始执行定时抓取任务...")
+    Logger.info("达到指定时间，开始执行 HN 爬取任务...")
 
     # 异步执行以免阻塞 GenServer
     Task.start(fn ->
       do_crawl(:news)
       do_crawl(:newest)
-      Logger.info("定时抓取任务完成。")
+      Logger.info("HN 定时抓取任务完成。")
     end)
 
-    Process.send_after(self(), :crawl, @interval)
+    schedule_next_run()
     {:noreply, state}
   end
 
   def run_now do
     send(__MODULE__, :crawl)
+  end
+
+  defp schedule_next_run do
+    now_utc = DateTime.utc_now()
+    current_hour = now_utc.hour
+
+    next_hour = Enum.find(@target_utc_hours, fn h -> h > current_hour end)
+
+    {target_date, target_hour} =
+      if next_hour do
+        {now_utc, next_hour}
+      else
+        # 跨天
+        {DateTime.add(now_utc, 1, :day), hd(@target_utc_hours)}
+      end
+
+    target_time = %{target_date | hour: target_hour, minute: 0, second: 0, microsecond: {0, 0}}
+    diff_ms = DateTime.diff(target_time, now_utc, :millisecond)
+    delay = max(diff_ms, 1000)
+
+    bjt_hour = rem(target_hour + 8, 24)
+    Logger.info("HN 爬虫已调度，将于北京时间 #{bjt_hour}:00 执行 (延迟 #{div(delay, 60_000)} 分钟)")
+
+    Process.send_after(self(), :crawl, delay)
   end
 
   defp do_crawl(source_type) do
